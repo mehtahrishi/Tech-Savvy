008ab20609e0eca1839bd50301e4f12d80255279
Hrishi <mehtahrishi45@gmail.com>
2025-01-21 21:06:30 +0530

    fresh

---
+++ src/app/Components/ChatBot.js
@@ -0,0 +1,9 @@
+import Chatbot from "react-chatbot-kit";
+import 'react-chatbot-kit/build/main.css'
+
+const ChatBot = () =>{
+    return(
+        <p> Hrishi </p>
+    )
+}
+export default ChatBot;
