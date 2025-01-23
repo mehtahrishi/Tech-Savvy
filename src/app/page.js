06e99991165bf39ddda6f3441312ec350dfa7a18
Hrishi <mehtahrishi45@gmail.com>
2025-01-23 18:14:15 +0530

    fresh

--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,15 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
+    <div style={{ backgroundColor: 'DodgerBlue', padding: '10px' }}>
+    {/* Chatbot content */}
+
     <div className={styles.page}>
       <main className={styles.main}>
         <ChatComponent />
         </main>
+        
+    </div>
     </div>
   );
 }
