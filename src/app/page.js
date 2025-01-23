d5743385a09fdbdd677c3aedf0ece1e39fda3e68
Hrishi <mehtahrishi45@gmail.com>
2025-01-23 21:58:16 +0530

    Description of your changes

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div style={{ backgroundColor: 'black', padding: '10px' }}>
+    <div style={{ backgroundColor: 'DodgerBlue', padding: '10px' }}>
     {/* Chatbot content */}
 
     <div className={styles.page}>
